0b76fd5b7999a347e516c84be32b35ca1b13954e
emioiso <jijjjiijiijijiji@gmail.com>
2024-04-21 19:30:44 +0900

    ログイン画面の見た目を作りました。

---
+++ ponponStamp/loginViewController.swift
@@ -0,0 +1,29 @@
+//
+//  loginViewController.swift
+//  ponponStamp
+//
+//  Created by emi oiso on 2024/04/21.
+//
+
+import UIKit
+
+class loginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
