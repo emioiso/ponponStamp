958034e7eaf59eb77667c7873242e34646ef6014
emioiso <jijjjiijiijijiji@gmail.com>
2024-04-22 18:19:39 +0900

    companyViewControllerの見た目を作りました。

---
+++ ponponStamp/companyViewController.swift
@@ -0,0 +1,29 @@
+//
+//  companyViewController.swift
+//  ponponStamp
+//
+//  Created by emi oiso on 2024/04/22.
+//
+
+import UIKit
+
+class companyViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
